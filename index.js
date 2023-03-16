337c749998b72b719a8e3c4bbad40e6393878593
Pavel Vakarash <pavelvakaras@gmail.com>
2023-03-16 16:27:28 +0100

    release

--- index.js
+++ index.js
@@ -9,7 +9,15 @@ btn.addEventListener('click', () => {
   // при нажатии на первую кнопку
   // работайте с clonedBtn
   // ваш код начинается здесь
-  
-})
+  document.body.appendChild(clonedBtn);
+  clonedBtn.textChange = "Я изменю тебя";
+  clonedBtn.style.backgroundColor = "#a78b71";
+  clonedBtn.style.color = "white";
+});
 
-// здесь можете создать EventListener для второй кнопки
+  // здесь можете создать EventListener для второй кнопки
+  clonedBtn.addEventListener('click', () => {
+    btn.style.backgroundColor = "#9c4a1a";
+    btn.style.color = "black"
+ 
+});
