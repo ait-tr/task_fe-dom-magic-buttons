bbae26cc3dc66d5efbf626e38f42f39a009d7192
Elena Vdovina <vdovina.ev.1@gmail.com>
2023-05-07 00:59:52 +0200

    Release

--- index.js
+++ index.js
@@ -9,7 +9,14 @@ btn.addEventListener('click', () => {
   // при нажатии на первую кнопку
   // работайте с clonedBtn
   // ваш код начинается здесь
-  
+  clonedBtn.innerText="Я изменю тебя";
+  clonedBtn.style.background="#a78b71";
+  clonedBtn.style.color="white";
+  document.body.appendChild(clonedBtn);
 })
 
 // здесь можете создать EventListener для второй кнопки
+clonedBtn.addEventListener('click', () => {
+  btn.style.background="#9c4a1a";
+  btn.style.color="black";
+  })
