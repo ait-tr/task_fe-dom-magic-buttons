1ff943eb6f0dd32d72c09bc109f998760cef073a
Dina Migunova <migunova.dina@gmail.com>
2023-05-07 22:14:26 +0200

    Add new btn with styles

--- index.js
+++ index.js
@@ -5,11 +5,21 @@ const clonedBtn = btn.cloneNode(true);
 // задали id
 clonedBtn.id = "magic-btn-2";
 
+
 btn.addEventListener('click', () => {
   // при нажатии на первую кнопку
   // работайте с clonedBtn
   // ваш код начинается здесь
-  
+  document.body.appendChild(clonedBtn);
+  clonedBtn.innerText = "Я изменю тебя";
+
+  clonedBtn.style.backgroundColor = "#a78b71";
+  clonedBtn.style.color = "white";
 })
 
 // здесь можете создать EventListener для второй кнопки
+
+clonedBtn.addEventListener('click', () => {
+  btn.style.backgroundColor = "#9c4a1a";
+  btn.style.color = "black";
+})
