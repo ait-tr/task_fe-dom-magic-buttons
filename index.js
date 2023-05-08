704ca4b0cbabf2375bfc57341c410cd6d955a96b
Dmitri Mandaji <badmopss@gmail.com>
2023-05-08 16:45:03 +0200

    add a Magic Button

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
-// найдем первую кнопку по id
-const btn = document.getElementById("magic-btn");
-// скопируем ее - получилась вторая кнопка. Ее можно использовать.
-const clonedBtn = btn.cloneNode(true);
-// задали id
-clonedBtn.id = "magic-btn-2";
+function addMagicButton() {
+  var magicButton = document.createElement("button");
+  magicButton.id = "magic-btn-2";
+  magicButton.innerText = "Я изменю тебя";
 
-btn.addEventListener('click', () => {
-  // при нажатии на первую кнопку
-  // работайте с clonedBtn
-  // ваш код начинается здесь
-  
-})
+  magicButton.style.backgroundColor = "#a78b71";
+  magicButton.style.color = "white";
 
-// здесь можете создать EventListener для второй кнопки
+
+  var container = document.getElementById("text");
+  container.appendChild(magicButton);
+}
+var originalButton = document.getElementById("magic-btn");
+originalButton.addEventListener("click", addMagicButton);
