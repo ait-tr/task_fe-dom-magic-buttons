1a2602dab9ac543bc51b7e5635ff5fdf0c7b62e9
Tykhonenko Yana <tihonenkoyana@gmail.com>
2023-05-08 17:20:11 +0200

    add homework

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const btn = document.getElementById("magic-btn");
 const clonedBtn = btn.cloneNode(true);
 // задали id
 clonedBtn.id = "magic-btn-2";
-
-btn.addEventListener('click', () => {
-  // при нажатии на первую кнопку
-  // работайте с clonedBtn
-  // ваш код начинается здесь
   
+  btn.addEventListener("click", () => {
+    document.body.appendChild(clonedBtn);
+    clonedBtn.textContent = "Я изменю тебя";
+    clonedBtn.style.backgroundColor = "#a78b71";
+    clonedBtn.style.color = "white";
+    
+  });
+
+  clonedBtn.addEventListener("click", () => {
+    btn.style.backgroundColor = "#9c4a1a";
+    btn.style.color = "black";
+
 })
 
-// здесь можете создать EventListener для второй кнопки
