d2da9f079d40ce588bea6fabc2db263ff5472982
Leon Zuev <loonatune@gmail.com>
2023-05-08 19:37:34 +0300

    Correct final solution

--- index.js
+++ index.js
@@ -10,22 +10,25 @@ btn.addEventListener('click', () => {
   // работайте с clonedBtn
   // ваш код начинается здесь
   document.body.appendChild(clonedBtn);
+  console.log('tuk tuk'); // проверка - вывод на консоль 'tuk tuk'
+  console.log(clonedBtn);
   clonedBtn.textContent = "Я изменю тебя";
   clonedBtn.style.backgroundColor = "#a78b71";
   clonedBtn.style.color = "white";
-  clonedBtn.style.borderRadius = "10px";
-  clonedBtn.style.border = "none";
-  clonedBtn.style.padding = "15px";
-  clonedBtn.style.margin = "20px";
-  clonedBtn.style.fontSize = "20px";
   clonedBtn.style.cursor = "pointer";
+  // обращаемся к классу
+  // clonedBtn.className = "second-btn";
+  clonedBtn.setAttribute("class", "second-btn"); // второй вариант
 })
+
 // здесь можете создать EventListener для второй кнопки
 clonedBtn.addEventListener('click', () => {
+  console.log('Akunamatata')
   // изменяем текст параграфа с id 'text'
   const text = document.getElementById('text');
   text.textContent = "Ты изменил меня!";
   text.style.color = "#f7c815";
-  btn.style.backgroundColor = "#a78b71";
+  btn.style.backgroundColor = "#9c4a1a";
+  btn.style.color = "#f7c815";
   clonedBtn.style.backgroundColor = "#f7c815";
 })
