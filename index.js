d56c74dd91ad890c2a331f5618d5160150d14a87
Oleksandr Zhurba <starkyzh@gmail.com>
2023-07-06 10:39:31 +0200

    initial commit

--- index.js
+++ index.js
@@ -4,12 +4,18 @@ const btn = document.getElementById("magic-btn");
 const clonedBtn = btn.cloneNode(true);
 // задали id
 clonedBtn.id = "magic-btn-2";
+clonedBtn.className = "second-btn";
+clonedBtn.innerText = "Я изменю тебя";
+clonedBtn.style.backgroundColor = "#a78b71";
+clonedBtn.style.color = "white";
 
 btn.addEventListener('click', () => {
-  // при нажатии на первую кнопку
-  // работайте с clonedBtn
-  // ваш код начинается здесь
-  
+  document.body.appendChild(clonedBtn);  
 })
 
 // здесь можете создать EventListener для второй кнопки
+clonedBtn.addEventListener('click', () => {
+  btn.style.backgroundColor = "#9c4a1a";
+  btn.style.color = "black";    
+})
+
