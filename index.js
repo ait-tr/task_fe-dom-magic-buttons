7141fe734155e50ccf43f040de84a1cf6534486f
Nathalie Russo <nrnatusia7@gmail.com>
2023-09-27 13:39:45 +0300

    add magic-btn

--- index.js
+++ index.js
@@ -6,10 +6,20 @@ const clonedBtn = btn.cloneNode(true);
 clonedBtn.id = "magic-btn-2";
 
 btn.addEventListener('click', () => {
-  // при нажатии на первую кнопку
-  // работайте с clonedBtn
-  // ваш код начинается здесь
-  
+  clonedBtn.innerText = "Я изменю тебя"
+  clonedBtn.style.color = 'white'
+  clonedBtn.style.backgroundColor = '#a78b71'
+  document.body.appendChild(clonedBtn)
 })
 
-// здесь можете создать EventListener для второй кнопки
+clonedBtn.addEventListener('click', () =>{
+  const elem = document.getElementById("magic-btn-2")
+  elem.id = "elem";
+  elem.style.color = 'black'
+  elem.style.backgroundColor = '#9c4a1a'
+  document.body.appendChild(elem)
+})
+
+
+
+
