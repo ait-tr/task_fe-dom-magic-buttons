9c7fc5b316fbc06404bfefe5edf27d28103cda09
knesp <knesp@Yandex.ru>
2023-10-31 21:07:22 +0400

    add second button that changes style of first button

--- index.js
+++ index.js
@@ -5,11 +5,23 @@ const clonedBtn = btn.cloneNode(true);
 // задали id
 clonedBtn.id = "magic-btn-2";
 
-btn.addEventListener('click', () => {
-  // при нажатии на первую кнопку
-  // работайте с clonedBtn
-  // ваш код начинается здесь
-  
-})
+let btnToggle = true;
 
-// здесь можете создать EventListener для второй кнопки
+btn.addEventListener('click', () => {
+  clonedBtn.classList.add("second-btn");
+  clonedBtn.style.backgroundColor = "#a78b71";
+  clonedBtn.style.color = "white";
+  clonedBtn.textContent = "Я изменю тебя";
+  document.body.appendChild(clonedBtn);
+});
+
+clonedBtn.addEventListener('click', () => {
+  if (btnToggle) {
+    btn.style.backgroundColor = "#9c4a1a";
+    btn.style.color = "black";
+  } else {
+    btn.style.backgroundColor = "#f7c815";
+    btn.style.color = "white";
+  }
+  btnToggle = !btnToggle;
+});
