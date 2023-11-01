5b80be1564241c6b83969ac146727621ddf2d57b
AGaleea <98322303+AGaleea@users.noreply.github.com>
2023-11-01 09:58:27 +0100

    js slightly modified

--- index.js
+++ index.js
@@ -6,16 +6,16 @@ const clonedBtn = btn.cloneNode(true);
 // задали id
 clonedBtn.id = "magic-btn-2";
 clonedBtn.classList.add("second-btn");
+clonedBtn.textContent = "Я изменю тебя";
+clonedBtn.style.backgroundColor = "#a78b71";
+clonedBtn.style.color = "white";
 
 
 let toggle = true;
 btn.addEventListener('click', () => {
   if(toggle) {
     document.body.appendChild(clonedBtn);
-    clonedBtn.textContent = "Я изменю тебя";
-    clonedBtn.style.backgroundColor = "#a78b71";
-    clonedBtn.style.color = "white";
-
+    
     clonedBtn.addEventListener('click', () => {
       clonedBtn.style.backgroundColor = "#9c4a1a";
       clonedBtn.style.color = "black";
