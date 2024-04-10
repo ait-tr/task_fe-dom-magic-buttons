bcf594a5657ae6149ab5a53e978f180875b382ad
Sergey Shvets <serega.shvets21@gmail.com>
2024-04-10 14:44:07 +0200

    new border

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ btn.addEventListener('click', () => {
 clonedBtn.textContent = "Я изменю тебя";
 clonedBtn.style.backgroundColor = "#a78b71";
 clonedBtn.style.color = "white";
+clonedBtn.style.border = "2px solid black";
 
 clonedBtn.addEventListener("click", ()=>{
   btn.style.backgroundColor = "#9c4a1a";
