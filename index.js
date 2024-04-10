2f1357eb656201353bebe6aa34178b045f831c0d
Taras <taraschaikovskii556@gmail.com>
2024-04-11 01:20:50 +0200

    First

--- index.js
+++ index.js
@@ -6,10 +6,14 @@ const clonedBtn = btn.cloneNode(true);
 clonedBtn.id = "magic-btn-2";
 
 btn.addEventListener('click', () => {
+    
+    document.body.append(clonedBtn);
+    clonedBtn.textContent = "Я изменю тебя";
+    clonedBtn.style.backgroundColor = "#a78b71";
+    clonedBtn.style.color="white";
   // при нажатии на первую кнопку
   // работайте с clonedBtn
   // ваш код начинается здесь
-  
 })
 
 // здесь можете создать EventListener для второй кнопки
