62706c8c9c4d3359ad8cb502cb63cd25fa69f140
Sergey Shvets <serega.shvets21@gmail.com>
2024-04-11 09:57:54 +0200

    toggle

--- index.js
+++ index.js
@@ -7,17 +7,23 @@ clonedBtn.id = "magic-btn-2";
 
 btn.addEventListener('click', () => {
   // при нажатии на первую кнопку
-  // работайте с clonedBtn
-  // ваш код начинается здесь
-  newElement = document.body.appendChild(clonedBtn)
+ clonedBtn.textContent = "я изменю тебя";
+ clonedBtn.style.backgroundColor = "#a78b71";
+  clonedBtn.style.color= "white";
+  clonedBtn.classList.add("second-btn");
+  document.body.append(clonedBtn)
 })
-clonedBtn.textContent = "Я изменю тебя";
-clonedBtn.style.backgroundColor = "#a78b71";
-clonedBtn.style.color = "white";
-clonedBtn.style.border = "2px solid black";
 
-clonedBtn.addEventListener("click", ()=>{
-  btn.style.backgroundColor = "#9c4a1a";
-  btn.style.color = "black"; 
-})
 // здесь можете создать EventListener для второй кнопки
+
+let toggle = true;
+clonedBtn.addEventListener("click",()=>{
+  if (toggle){
+    btn.style.backgroundColor = "#9c4a1a";
+    btn.style.color="black";
+  }else{
+    btn.style.backgroundColor = "#f7c815";
+    btn.style.color="white";
+  }
+  toggle = !toggle
+})
